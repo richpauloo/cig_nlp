964b711591aa06f2957e42d688da7b4cb1c12785
Jane Carlen <janecarlen@campus-005-101.ucdavis.edu>
2019-04-04 15:14:09 -0700

    Added package-specific plots to code and added the new authors/papers to the old plots

--- code/aspect_author_plot.R
+++ code/aspect_author_plot.R
@@ -1,10 +1,12 @@
 # Code to make a network plot of ASPECT authors
 # Jane Carlen, February 2019
 
-# TO DO:
+# TO DO:s
 # more network analysis. Path lengths? Clustering coefficient? Over time?
+# When I load bibtek I get message "Some BibTeX entries may have been dropped. The result could be malformed. Review the .bib file and make sure every single entry starts with a '@'." Doesn't say which.
 
-# 1. Setup  ----
+# ------------------------------------------------------------------------------------------
+# 0. Setup  ----
 
 library(readxl)
 library(bibtex)
@@ -19,16 +21,26 @@ library(forcats)
 library(reshape2)
 library(visNetwork)
 library(RColorBrewer)
+library(grDevices)
 
 setwd("~/Documents/DSI/cig_citation/")
 
-# 2. Aspect data  ----
+# ---------------------------- Original Data for Talk ---------------------------------------
+# 1. Aspect data  ----
 
 authors = read_xlsx("protoNetworkA_jc_edited.xlsx", sheet = 3); head(authors)
 
 # Late-add authors added below
 #J. Perry-Houts  (hack Participant 2015) & L. Karlstrom are co-authors
 #L. Schuurmans. Thesis (no co-authors)
+
+# from: https://docs.google.com/document/d/1jCXNRsIAjVjS5Dz2PqwhZKlsodXaKSXbdz9jQArCHso/edit
+# "Status: ASPECT - NEED TO ADD"
+
+# Three more papers with the following co-authors:
+# Eva Bredow	Single author. This is her dissertation
+# Giacomo Corti,, Raffaello Cioni, Zara Franceschini, Federico Sani, Stéphane Scaillet, Paola Molin, Ilaria Isola, Francesco Mazzarini, Sascha Brune , Derek Keir, Asfaw Erbello, Ameha Muluneh, Finnigan Illsley-Kemp, Anne Glerum
+# S. Liu and S.D. King
 
 # -  Clean author names  ----
 aspect.bib = read.bib("bibtex_export only pubs.bib", encoding = "latin1") #had to add school placeholder 'test' for some articles so they'd load
@@ -53,6 +65,10 @@ sort(table(unlist(aspect.authors.list)))
 #L. Schuurmans. Thesis (no co-authors)
 aspect.authors.list$"Perry-Houts+Karlstrom2018" = c("J. Perry-Houts", "L. Karlstrom")
 aspect.authors.list$"Schuurmans2018" = "L. Schuurmans"
+# from: https://docs.google.com/document/d/1jCXNRsIAjVjS5Dz2PqwhZKlsodXaKSXbdz9jQArCHso/edit
+aspect.authors.list$"Bredow" = "E. Bredow"
+aspect.authors.list$"Corti+etal" = c("G. Corti, R. Cioni, Z. Franceschini, F. Sani, S. Scaillet, P. Molin, I. Isola, F. Mazzarini, S. Brune, D. Keir, A. Erbello, A. Muluneh, F. Illsley-Kemp, A. Glerum")
+aspect.authors.list$"Liu+King" = c("S. Liu", "S.D. King")
 
 # -  Convert to co-author network  ----
 author.pairs = lapply(aspect.authors.list, function(x) { if(length(x) > 1) {t(combn(x, 2))} else {NULL}})
@@ -99,7 +115,7 @@ ggsave(plot = author.plot, "aspect_author_plot_no_labels.png", height = 10, widt
 author.plot = author.plot + geom_node_text(aes(label = name), repel = TRUE, segment.alpha = .5, fontface = "bold")
 ggsave(plot = author.plot, "aspect_author_plot.png", height = 10, width  = 10)
 
-# 3. Hack data  ----
+# 2. Hack data  ----
 
 hack = read_xlsx("protoNetworkA_jc_edited.xlsx", sheet = 4)[,1:7] #only care about hacks in first columns, not workships
 dim(hack)
@@ -197,7 +213,7 @@ hack.plot = hack.plot + geom_node_text(aes(label = name), repel = TRUE, segment.
 ggsave(plot = hack.plot, "hack_plot.png", height = 10, width  = 14)
 
   
-# 4. Make combined net ----
+# 3. Make combined net ----
 
 # Merged hacks into aggregate so it's not too busy
 names(author.pairs)[3] = "co_" #make co_* name match so we can rbind
@@ -255,7 +271,7 @@ ggsave(plot = geo.plot, "author_and_hack_plot_no_labels.png", height = 10, width
 geo.plot = geo.plot + geom_node_text(aes(label = name), repel = TRUE, segment.alpha = .5, fontface = "bold")
 ggsave(plot = geo.plot, "author_and_hack_plot.png", height = 10, width  = 14)
 
-# 5. Centrality measures ####
+# 4. Centrality measures ####
 
 # Preliminary work, need to check how these igraph measures account for disconnected components, valued edges and mutli-edges
 
@@ -290,21 +306,332 @@ ggplot(melt(centrality_measures, idvars = c('name', 'degree_rank'), value.name =
 ggsave(title1, device = "png")
   
 }
-# 6. Interactive (draft) ####
+# 5. Interactive (draft) ####
 
 net1 = geo.net
-V(net1)$color =  c(brown.rbg, red.rgb, "gray")[as.numeric(as.factor(V(net1)$Author_type))]
+V(net1)$color =  c(brown.rgb, red.rgb, "gray")[as.numeric(as.factor(V(net1)$Author_type))]
 V(net1)$color[V(net1)$name %in% V(hack.net)$name & V(net1)$color == "gray"] = "green" 
 V(net1)$label = V(net1)$name
 V(net1)$label.color = "black"
 V(net1)$size= 6
 V(net1)$label.cex = .3
 
-visIgraph(net1, idToLabel= FALSE, layoutMatrix = layoutMatrix1) %>%
+visIgraph(net1, layout = "layout.norm", idToLabel= FALSE, layoutMatrix = as.matrix(layoutMatrix1)) %>%
   visOptions(highlightNearest = list(enabled = TRUE, algorithm = "all", degree = list(from = 0, to = 0)),
                nodesIdSelection = list(enabled = TRUE)) %>%
   visInteraction(dragNodes = TRUE, 
                  dragView = TRUE, 
                  zoomView = TRUE)
+
+
+
+#----------------------------- Complete Data for Paper  -------------------------------------
+
+# NOTE:
+# Any names that should have a different spelling, etc should be changed in cig_allauthors_JAC 2
+# Names that are incorrectly formatted, jumbled or grouped with others should be fixed directly in the bibtek
+
+# 1b. Read in bibtek files (older) to make list of author names for cleaning ----
+
+#devtools::install_github("https://github.com/ropensci/bib2df")
+library(bib2df)
+
+#have to get multiple authors by hand because code doesn't expect authors with "and" to new line
+bib_2_df <- function(file) {
+  bib <- readLines(file)
+  bib <- paste(bib, collapse = "\n")
+  bib = gsub(bib, pattern = "\nand", replacement = " and ")
+  bib = str_split(bib, "\n")[[1]]
+  bib = bib2df:::bib2df_gather(bib) #from inside bib2
+  return(bib)
+}
+
+# Older versions were used to make author name spreadsheet then cleaned by Lorraine
+cig_2015 = bib_2_df(file = "data/2015 bibtex_export v2.bib")
+cig_2016 = bib_2_df(file = "data/2016 bibtex_export v2.bib")
+cig_2017 = bib_2_df(file = "data/2017 bibtex_export v3.bib")
+cig_2018 = bib_2_df(file = "data/2018 bibtex_export v3.bib")
+
+# make one big data frame
+allnames = c(names(cig_2015), names(cig_2016), names(cig_2017), names(cig_2018))
+keepnames = names(table(allnames))[table(allnames)==4]
+
+# use columns in all years
+cig_allyears = rbind(cig_2015[,keepnames], cig_2016[,keepnames], cig_2017[,keepnames], cig_2018[,keepnames])
+
+# only keep colums with data 
+cig_allyears  = cig_allyears[,!apply(cig_allyears, 2, function (x) sum(!is.na(x)))==0]
+
+#Make new author fields to go in spreadsheet
+max.authors = max(sapply(cig_allyears$AUTHOR, function(x) {length(unlist(str_split(x, ",")))}))
+allauthors = data.frame(nrow(cig_allyears), max.authors)
+allauthors = t(do.call("cbind", lapply(str_split(cig_allyears$AUTHOR, " and "), "[", 1:max.authors)))
+colnames(allauthors) = paste("AUTHOR_", 1:64, sep = "")
+cig_allyears = cbind(cig_allyears, allauthors)
+write.csv(cig_allyears, "cig_allyears.csv")
+write.csv(table(allauthors), "cig_allauthors.csv")
+
+# 2b. Read in bibtek files (newest) to make plot ----
+
+# FIXED latest bibtek files based on "Instructions" column in cig_all_authors_JAC 2.csv (based on Lorraine's version of that file) except Li. P said "THROW THIS ONE OUT" why? I just changed it to Li, P.
+
+# Lorrain's list for the analysis (intentionally skipped 2016)
+    # https://docs.google.com/document/d/1jCXNRsIAjVjS5Dz2PqwhZKlsodXaKSXbdz9jQArCHso/edit
+    # "These should have the .bibs for the years 2010-2018."
+
+cig_2015 = bib_2_df(file = "data/2015 bibtex_export v4.2.bib") #Higher version numbers reflect my final cleaning
+cig_2017 = bib_2_df(file = "data/2017 bibtex_export v5.2.bib")
+cig_2018 = bib_2_df(file = "data/2018 bibtex_export v5.2.bib")
+
+#Add dummy  columns to 2017 and 2018 for keywordds
+optkeywordvars = names(cig_2015[grepl("OPTKEYWORDS", x = names(cig_2015))])
+optkeywordvars[!optkeywordvars %in% names(cig_2017)]
+optkeywordvars[!optkeywordvars %in% names(cig_2018)]
+cig_2017 = cig_2017 %>% mutate("OPTKEYWORDS.2" = NA,
+                               "OPTKEYWORDS.3" = NA,
+                               "OPTKEYWORDS.4" = NA,
+                               "OPTKEYWORDS.5" = NA)
+cig_2018 = cig_2018 %>% mutate("OPTKEYWORDS.2" = NA,
+                               "OPTKEYWORDS.3" = NA,
+                               "OPTKEYWORDS.4" = NA,
+                               "OPTKEYWORDS.5" = NA)
+
+# Find common column names so we can rbind
+allnames = c(names(cig_2015), names(cig_2017), names(cig_2018))
+keepnames = names(table(allnames))[table(allnames)==3]
+
+# use columns in all years
+cig_allyears = rbind(cig_2015[,keepnames], cig_2017[,keepnames], cig_2018[,keepnames])
+cig_allyears  = cig_allyears[,!apply(cig_allyears, 2, function (x) sum(!is.na(x)))==0]
+write.csv(cig_allyears, "cig_allyears_v5.csv", row.names = F)
+
+# Do we only want articles?
+table(cig_allyears$CATEGORY)
+
+# -  Clean author names  ----
+
+# Author names always separted by " and "
+split_names = sapply(cig_allyears$AUTHOR, strsplit, " and ")
+split_names = sapply(split_names, trimws)
+unique_names = unique(unlist(split_names))
+  
+# -  Use spreadsheet of fixed names to unify them ----
+final_names = read.csv("cig_allauthors_JAC 2.csv", stringsAsFactors = F)
+modified_names = filter(final_names, FinalName!="" |  Instructions!="" | Note.to.Self!="" )[,2:6]
+#View(modified_names)
+
+# Which names have replacements in final_names?
+sum(unique_names %in%  modified_names$allauthors)
+# View(filter(modified_names,  modified_names$allauthors %in% unique_names) %>%
+#  filter(!duplicated(allauthors)))
+modified_name_dict = (filter(modified_names,  modified_names$allauthors %in% unique_names) %>%
+  filter(!duplicated(allauthors)))[,c("allauthors", "FinalName")]
+modified_name_dict[modified_name_dict$FinalName == "", "FinalName"] =
+  modified_name_dict[modified_name_dict$FinalName == "", "allauthors"]
+
+# Replace names with modified as necessary
+split_names2 = lapply(split_names, function(x) {x[x %in% modified_name_dict$allauthors] = 
+  modified_name_dict$FinalName[modified_name_dict$allauthors %in% x]
+  x})
+
+# -  Convert to co-author network  ----
+
+author.pairs = lapply(split_names2, function(x) { if(length(x) > 1) {t(combn(x, 2))} else {NULL}})
+author.pairs = data.frame(do.call("rbind", author.pairs))
+author.pairs$count = 1
+author.pairs = aggregate(author.pairs$count, list(author.pairs$X1, author.pairs$X2), sum)
+names(author.pairs)[3] = "co_authorships"
+write.csv(author.pairs, "author_pairs_final.csv", row.names = F)
+author.net = graph_from_data_frame(author.pairs, directed = F, 
+                                   vertices = as.vector(unique(unlist(split_names2))))
+plot(author.net)
+
+# -  Make plot and add colors ----
+
+# Special authors
+V(author.net)$name_last = gsub(x = sapply(str_split(V(author.net)$name, " "), first), ",", "")
+red = c("Bangerth", "Heister", "Gassm.*ller", "Dannberg") #Original Lead Developers
+brown = c("Glerum", "Fraters", "Austermann", "Naliboff") #Added Principal Developers
+color.aspect = c("Other", "Original Lead Developers")[1 + rowSums(sapply(red, grepl, vertex_attr(author.net, "name")))]
+color.aspect[V(author.net)$name_last%in% brown] = "Added Principal Developers"
+red.rgb = "#990033"
+brown.rgb = "#ff5050"
+V(author.net)$Author_type =  color.aspect
+V(author.net)$Papers = table(unlist(split_names2))[V(author.net)$name]
+E(author.net)$co_authorships
+  
+author.plot = ggraph(author.net, layout = "kk") + 
+  geom_edge_link(aes(width = co_authorships), alpha = 1, color = "gray") +
+  # for some reason edge width not working like it doesn in the plots below
+  scale_edge_width(name = "Number of co-authorships") + #, range=c(.2,2.8), breaks = c(1,2,3)) +
+  geom_node_point(aes(fill = Author_type, size = Papers, text = name), shape = 21, stroke = .5, alpha = .5) + 
+  scale_fill_manual(values =  c(red.rgb, brown.rgb, "white"), 
+                    guide = guide_legend(title = "Author_type", order = 1)) + 
+  theme_bw() +
+  theme(axis.text.x = element_blank(), axis.text.y = element_blank(),
+        axis.title.x = element_blank(), axis.title.y = element_blank(),
+        axis.ticks.x = element_blank(), axis.ticks.y = element_blank(),
+        panel.grid = element_blank()) +
+  ggtitle("Co-author Relationships 2010 - 2018")
+
+warnings() #unknown aes text is for the plotly below
+author.plot
+
+# interactive plot instead of labels ----
+# Too many nodes for labels
+
+# plotly::ggplotly(author.plot) # can't do edges yet
+
+#for visNetwork
+V(author.net)$color = c(red.rgb, brown.rgb, rgb(0,0,0,.3))[as.factor(V(author.net)$Author_type)]
+V(author.net)$title = V(author.net)$name
+layoutMatrix1 = author.plot$data[,1:2]
+
+author.visNet = visIgraph(author.net, layout = "layout.norm", idToLabel= TRUE, 
+          layoutMatrix = as.matrix(layoutMatrix1)) %>% 
+  visOptions(highlightNearest = list(enabled = TRUE, algorithm = "hierarchical", hover = FALSE,
+                                     degree = 1, hideColor = rgb(0,1,0,.05)),
+             nodesIdSelection = list(enabled = TRUE, main = "Select author",
+                                     values = sort(V(author.net)$title))) %>%
+  visInteraction(dragNodes = TRUE, 
+                 dragView = TRUE, 
+                 zoomView = TRUE,
+                 multiselect = TRUE)
+
+  
+author.visNet$x$main = list(text = "Co-author Relationships 2010 - 2018", style = "font-family: Arial, Helvetica, sans-serif;font-size:16px;text-align:center;")
+#author.visNet$x$nodes$color = list(hover = "#000000")
+warnings()
+author.visNet
+
+#----------------------------- Keyword plots  ------------------------------------------------
+
+# To do list instructions say "Create for all software packages." 
+# filter by a certain keyword
+
+# i'll ignore case in searching for these
+packagelist = c(
+"ASPECT", #focus
+"Ellipsis3d",
+"SEISMIC.*CPML", #had to add slash to remove error
+"AxiSEM",
+"FLEXWIN",
+"SELEN",
+"Burnman",
+"Gale", 
+"SNAC",
+"Calypso",
+"MAG", 
+"SPECFEM", #focus - anything starting with specfem*
+"CitcomCU",
+"CitcomS", # CitcomCU, CitcomS - seperate and together plot. In together plot code authors by whether they’re CU, S or both
+"Citcom", # For together plot
+"Mineos",
+"SW4",
+"PyLith", #focus
+#"Virtual California", #Change to Virtual Quake?
+"ConMan",
+"RELAX")
+
+# subset plot ----
+for (package in packagelist) {
+  
+  print(package)
+  
+  # subset data 
+  subset_index = colSums(apply(cig_allyears %>% select(contains("OPTKEYWORDS")), 1, 
+                               grepl, pattern = package, ignore.case = TRUE)) > 0
+  
+  split_names_sub = split_names2[subset_index]
+  
+  if (package == "Citcom") {
+    subset_index1 = colSums(apply(cig_allyears %>% select(contains("OPTKEYWORDS")), 1, 
+                                 grepl, pattern = "CitcomS", ignore.case = TRUE)) > 0
+    split_names_sub1 = split_names2[subset_index1]
+    
+    subset_index2 = colSums(apply(cig_allyears %>% select(contains("OPTKEYWORDS")), 1, 
+                                  grepl, pattern = "CitcomCU", ignore.case = TRUE)) > 0
+    split_names_sub2 = split_names2[subset_index2]
+    
+    unique(unlist(split_names_sub2)) %in% unique(unlist(split_names_sub1))
+    
+    split_names_sub = split_names2[subset_index1 | subset_index2]
+    
+    
+  }
+  
+  # subset net 
+  author.pairs = lapply(split_names_sub, function(x) { if(length(x) > 1) {t(combn(x, 2))} else {NULL}})
+  author.pairs = data.frame(do.call("rbind", author.pairs))
+  author.pairs$count = 1
+  author.pairs = aggregate(author.pairs$count, list(author.pairs$X1, author.pairs$X2), sum)
+  names(author.pairs)[3] = "co_authorships"
+  package.net = graph_from_data_frame(author.pairs, directed = F, 
+                                     vertices = as.vector(unique(unlist(split_names_sub))))
+  V(package.net)$Papers = table(unlist(split_names_sub))[V(package.net)$name]
+  E(package.net)$co_authorships
+  
+  # if ASPECT, special authors/colors 
+  if (package == "ASPECT") {
+    V(package.net)$name_last = gsub(x = sapply(str_split(V(package.net)$name, " "), first), ",", "")
+    red = c("Bangerth", "Heister", "Gassm.*ller", "Dannberg") #Original Lead Developers, gassmoller has weird     characters
+    brown = c("Glerum", "Fraters", "Austermann", "Naliboff") #Added Principal Developers
+    color.aspect = c("Other", "Original Lead Developers")[1 + rowSums(sapply(red, grepl, vertex_attr(package.net, "name")))]
+    color.aspect[V(package.net)$name_last%in% brown] = "Added Principal Developers"
+    red.rgb = "#990033"
+    brown.rgb = "#ff5050"
+    V(package.net)$Author_type =  color.aspect
+  }
+  # if Citcom, special authors/colors
+  if (package == "Citcom") {
+    tmp = V(package.net)$name %in% unique(unlist(split_names_sub1)) 
+    tmp[tmp] = "Both"
+    tmp[V(package.net)$name %in% unique(unlist(split_names_sub2)) ] = "CitcomS"
+    tmp[V(package.net)$name %in% unique(unlist(split_names_sub1)) & 
+          V(package.net)$name %in% unique(unlist(split_names_sub2)) ] = "CitcomCU"
+    V(package.net)$Author_type = tmp
+  }
+  
+  #fix package name for title in some cases
+  if (package == "SEISMIC.*CPML") package = "SEISMIC_CPML" 
+  if (package == "Virtual.*California") package = "Virtual California"
+  
+  # plot
+  package.plot = ggraph(package.net, layout = "kk") + 
+    geom_edge_link(aes(width = co_authorships), alpha = 1, color = "gray") +
+    # for some reason edge width not working like it doesn in the plots below
+    scale_edge_width(name = "Number of co-authorships",
+                     range = c(.2, 2.8),
+                     breaks = 1:max(E(package.net)$co_authorships) 
+                     ) +
+    theme_bw() +
+    theme(axis.text.x = element_blank(), axis.text.y = element_blank(),
+          axis.title.x = element_blank(), axis.title.y = element_blank(),
+          axis.ticks.x = element_blank(), axis.ticks.y = element_blank(),
+          panel.grid = element_blank()) +
+    ggtitle(paste(package, "Co-author Relationships"))
+  
+  if (package == "ASPECT") {
+    package.plot = package.plot + 
+      geom_node_point(aes(fill = Author_type, size = Papers), color = "black", shape = 21,
+                      stroke = .5, alpha = .5) + 
+      scale_fill_manual(values =  c(red.rgb, brown.rgb, "white"),
+                      guide = guide_legend(title = "Author_type", order = 1))
+  } else if (package == "Citcom"){
+    package.plot = package.plot + 
+      geom_node_point(aes(fill = Author_type, size = Papers), color = "black", shape = 21,
+                      stroke = .5, alpha = .7) + 
+      scale_fill_manual(values =  c("darkred", "gray", "pink"),
+                        guide = guide_legend(title = "Author_type", order = 1))
+    
+  } else {
+    package.plot = package.plot + 
+      geom_node_point(aes(size = Papers), color = "black", fill, shape = 21,
+                    stroke = .5, alpha = .5)
+  }
+
+ggsave(package.plot, filename = paste0("package_plots/", package, "_co_author_relationships", ".png"))
+
+}
 
 
