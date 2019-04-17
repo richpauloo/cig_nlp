ee3035f1cbeca24f0ec90add95c86bce3ad64fab
Rich Pauloo <richpauloo@gmail.com>
2019-04-16 19:37:28 -0700

    Update 07_dois.R

--- code/07_dois.R
+++ code/07_dois.R
@@ -12,4 +12,5 @@ raw <- read_rds("C:/Users/rpauloo/Documents/Github/cig_nlp/rich_data/raw2.rds")
 
 dois <- sapply(raw, function(x) stringr::str_extract(x, pattern = "doi:[:digit:]{1,}.[:digit:]{1,}/[:alpha:]{1,}.[:digit:]{1,}"))
 
-data.frame(p, dois)
+
+View(data.frame(p, dois))
