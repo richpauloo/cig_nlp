92c236ead974f83c624cdb96a1f7c0f8bf2a21c3
Rich Pauloo <richpauloo@gmail.com>
2019-04-16 19:30:15 -0700

    Create 07_dois.R

---
+++ code/07_dois.R
@@ -0,0 +1,17 @@
+##################################################
+# Grab dois for each PDF
+##################################################
+
+library(readr)
+library(stringr)
+
+fp <- "F:/Box Sync/2019 CItation/Papers/" # PC
+
+# paper names
+p <- list.files(paste0(fp, "all_papers")) 
+
+raw <- read_rds("C:/Users/rpauloo/Documents/Github/cig_nlp/rich_data/raw2.rds")
+
+dois <- sapply(raw, function(x) stringr::str_extract(x, pattern = "doi:[:digit:]{1,}.[:digit:]{1,}/[:alpha:]{1,}.[:digit:]{1,}"))
+
+data.frame(p, dois)
